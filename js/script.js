6a44144091e56c7e5df126191e9545c186328c40
shrabon-dev <shrabonhossain.dev@gmail.com>
2022-04-16 13:49:39 +0600

    update design

--- js/script.js
+++ js/script.js
@@ -237,3 +237,34 @@
     $('.bar__opt').click(function(){
          $('.none__menu__bar').toggleClass('active__menu__bar');
     })
+
+
+/*
+See https://www.greensock.com/splittext/ for details. 
+This demo uses SplitText which is a membership benefit of Club GreenSock, https://www.greensock.com/club/
+*/
+gsap.registerPlugin(GSDevTools, SplitText)
+let split
+let animation = gsap.timeline({})
+
+function init() {
+	gsap.set(".wrapper", {autoAlpha:1})
+	split = new SplitText("h1", {type:"chars"})
+	animation.from(split.chars, {opacity:0, y:50, ease:"back(4)", stagger:{
+		from:"end", //try "center" and "edges"
+		each:0.05
+	}})
+	GSDevTools.create({animation:animation})
+}
+
+window.addEventListener("load", init)
+
+(function($){
+	"use strict";
+	$(document).ready( function () {
+		$('.testing').progressBar({
+			value: "70",
+			height: "35",
+		});
+	});
+})(jQuery);
